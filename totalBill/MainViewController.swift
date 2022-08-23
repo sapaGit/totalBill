19e0c383020cfe3078739959d0948b7b7091a75d
sapaGit <mosevaly@gmail.com>
2022-08-23 08:56:31 +0300

    01.2 Refactor VC, add two funcs

---
+++ totalBill/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  totalBill
+//
+//  Created by Sergey Pavlov on 23.08.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    func setupViews() {
+        
+    }
+
+}
+
+extension MainViewController {
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+        
+        
+        ])
+    }
+}
