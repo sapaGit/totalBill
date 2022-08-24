5d3ccc70ff031eea73b57e652e922091d8ef527a
sapaGit <mosevaly@gmail.com>
2022-08-24 16:22:01 +0300

    02.1 Add titleLabel, imageView without storyboard

--- totalBill/MainViewController.swift
+++ totalBill/MainViewController.swift
@@ -9,15 +9,35 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Total Bill"
+        label.textColor = .black
+        label.font = UIFont(name: "Avenir Next Bold", size: 40)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let logoImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+       
         setupViews()
         setConstraints()
     }
     
     func setupViews() {
+        view.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.9764705882, blue: 0.9764705882, alpha: 1)
         
+        view.addSubview(titleLabel)
+        view.addSubview(logoImageView)
     }
 
 }
@@ -25,8 +45,14 @@ class MainViewController: UIViewController {
 extension MainViewController {
     func setConstraints() {
         NSLayoutConstraint.activate([
-        
-        
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            logoImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            logoImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            logoImageView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            
         ])
     }
 }
