8579184cb54114cc9679b5d57bcb86473ef239bd
sapaGit <mosevaly@gmail.com>
2022-08-27 11:03:46 +0300

    04.7 Add selection logic to viewCell

--- totalBill/Views/Cells/TipsCollectionViewCell.swift
+++ totalBill/Views/Cells/TipsCollectionViewCell.swift
@@ -17,6 +17,17 @@ class TipsCollectionViewCell: UICollectionViewCell {
         return label
     }()
     
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                layer.borderWidth = 3
+                layer.borderColor = UIColor.purple.cgColor
+            } else {
+                layer.borderWidth = 0
+            }
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
